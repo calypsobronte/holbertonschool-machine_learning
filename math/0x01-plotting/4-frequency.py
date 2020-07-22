a7b4c5833fa2be9b518943270cbfaab9fbfa5562
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-07-22 04:48:55 +0000

    Plotting in Graph

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# your code here
+plt.hist(student_grades, bins = range(0, 110, 10), edgecolor = "black")
+plt.xticks(ticks=range(0,110,10))
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title("Project A")
+plt.show()
--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+y0 = np.arange(0, 11) ** 3
+
+mean = [69, 0]
+cov = [[15, 8], [8, 15]]
+np.random.seed(5)
+x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+y1 += 180
+
+x2 = np.arange(0, 28651, 5730)
+r2 = np.log(0.5)
+t2 = 5730
+y2 = np.exp((r2 / t2) * x2)
+
+x3 = np.arange(0, 21000, 1000)
+r3 = np.log(0.5)
+t31 = 5730
+t32 = 1600
+y31 = np.exp((r3 / t31) * x3)
+y32 = np.exp((r3 / t32) * x3)
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# your code here
+fig = plt.figure()
+fig.suptitle("All in One", fontsize = "x-large")
+grid = plt.GridSpec(3, 2, wspace=0.5, hspace=0.5)
+
+# 0
+plt.subplot(grid[0, 0])
+plt.plot(y, color = 'red')
+plt.show()
