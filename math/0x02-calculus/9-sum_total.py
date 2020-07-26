37599c8bcf8c2c04c7e121b47dfa8c7374fe09db
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-07-26 23:52:38 +0000

    Fix style for PEP8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ sum total of all the decisions """
 
+
 def summation_i_squared(n):
     """ calculates """
+
     if type(n) is not int or n < 1:
         return None
     return n * (n + 1) * ((n * 2) + 1) // 6
-
