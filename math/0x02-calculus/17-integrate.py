cbb7debf1e22e91795938522b983dc97f7237ec5
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-07-27 05:11:43 +0000

    a

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,6 +6,8 @@ def poly_integral(poly, C=0):
     """ calculate integral polynomial """
     if not isinstance(poly, list) or len(poly) == 0:
         return None
+    if not isinstance(C, (int, float)):
+        return None
     if len(poly) == 1:
         return [0]
     if poly == [0]:
